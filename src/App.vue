c87e78304b5439b22a8f33be2c270286240ed9c5
Matteo <lingio94@gmail.com>
2020-05-23 17:45:42 +0200

    grid system

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="app">
+    <Grid>
+      <div
+        v-for="i in Array.from({ length: 12 }, (x, i) => i)"
+        :key="i"
+        style="background-color: red;"
+        class="col sm-12 md-4 lg-3 xl-1"
+      ></div>
+    </Grid>
   </div>
 </template>
 
 <script>
+import Grid from "./components/Grid";
 
 export default {
-  name: "app"
+  name: "app",
+  components: { Grid }
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Poppins:wght@400;500;700&display=swap");
+@import "./assets/styles/_colors.scss";
+@import "./assets/styles/_typography.scss";
+
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  padding: 0 0 0;
+  font-family: $primary-font;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  display: flex;
+}
 </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="grid">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/_spacing.scss";
+@import "../assets/styles/_mixins.scss";
+
+.grid {
+  display: flex;
+  flex-direction: row;
+  background-color: blue;
+  height: 100vh;
+  width: 100%;
+  flex-wrap: wrap;
+
+  &--vertical {
+    flex-direction: column;
+  }
+}
+
+.col {
+  margin: $grid_margin;
+  &--no-gap {
+    margin: 0;
+  }
+}
+
+// @media (max-width: 768px) {
+//   @for $i from 1 through 12 {
+//     .sm-#{$i} {
+//       @include column-width($i);
+//     }
+//   }
+// }
+
+@include for-phone-only {
+  @for $i from 1 through 12 {
+    .sm-#{$i} {
+      @include column-width($i, false);
+    }
+  }
+}
+
+@include for-tablet-portrait-up {
+  @for $i from 1 through 12 {
+    .md-#{$i} {
+      @include column-width($i, false);
+    }
+  }
+}
+
+@include for-big-desktop-up {
+  @for $i from 1 through 12 {
+    .xl-#{$i} {
+      @include column-width($i, false);
+    }
+  }
+}
+
+@include for-desktop-up {
+  @for $i from 1 through 12 {
+    .lg-#{$i} {
+      @include column-width($i, false);
+    }
+  }
+}
+</style>
